5eede6c4a3b779fb2c8c76900fdc880a8b85998b
Bikal <bikaladhikari5@gmail.com>
2024-04-24 18:00:43 +1000

    morgan middleware added and started working on taskRouter

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
 import express from "express";
-
+import morgan from "morgan";
 import bodyParser from "body-parser";
 import cors from "cors";
+import taskRouter from "./src/routers/taskRouter.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
+app.use(morgan("dev"));
+
 app.use(cors());
 
-
-
-
-
-app.get("/api/posts", async (req, res) => {
-  try {
-    const posts = await Post.find();
-    res.json(posts);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching posts" });
-  }
-});
-
-app.post("/api/posts", async (req, res) => {
-  try {
-    const newPost = new Post(req.body);
-    await newPost.save();
-    res.status(201).json(newPost);
-  } catch (error) {
-    res.status(500).json({ message: "Error creating post" });
-  }
-});
+app.use("/", taskRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ src/routers/taskRouter.js
@@ -0,0 +1,18 @@
+app.get("/api/posts", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching posts" });
+  }
+});
+
+app.post("/api/posts", async (req, res) => {
+  try {
+    const newPost = new Post(req.body);
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating post" });
+  }
+});
