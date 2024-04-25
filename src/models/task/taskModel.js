adf37e0266b5393b9f963096c380375c5e6fcc97
Bikal <bikaladhikari5@gmail.com>
2024-04-25 16:16:42 +1000

    made changes in task model to add present date

--- src/models/task/taskModel.js
+++ src/models/task/taskModel.js
@@ -1,40 +1,51 @@
-// db queries go inside this file
-
 import TaskSchema from "./taskSchema.js";
 import { format } from "date-fns";
-// C
 
+// Create
 export const insertTask = async (taskObj) => {
   try {
     // Format the current date to dd/mm/yyyy format
     const currentDate = format(new Date(), "dd/MM/yyyy");
     taskObj.date = currentDate;
 
-    const newTask = new Task(taskObj);
+    const newTask = new TaskSchema(taskObj);
     const result = await newTask.save();
     return result;
   } catch (error) {
     throw new Error(`Error inserting task: ${error.message}`);
   }
-
-  // return TaskSchema(taskObj).save();
 };
 
-//R
-
-export const getTasks = () => {
-  return TaskSchema.find();
+// Read
+export const getTasks = async () => {
+  try {
+    const tasks = await TaskSchema.find();
+    return tasks;
+  } catch (error) {
+    throw new Error(`Error fetching tasks: ${error.message}`);
+  }
 };
 
-//U
-
-export const updateTask = ({ _id, type }) => {
-  return TaskSchema.findByIdAndUpdate(_id, { type }, { new: true });
+// Update
+export const updateTask = async ({ _id, type }) => {
+  try {
+    const updatedTask = await TaskSchema.findByIdAndUpdate(
+      _id,
+      { type },
+      { new: true }
+    );
+    return updatedTask;
+  } catch (error) {
+    throw new Error(`Error updating task: ${error.message}`);
+  }
 };
 
-//D
-
-//deleteMany
-export const deleteTask = (ids) => {
-  return TaskSchema.deleteMany({ _id: { $in: ids } });
+// Delete
+export const deleteTask = async (ids) => {
+  try {
+    const result = await TaskSchema.deleteMany({ _id: { $in: ids } });
+    return result;
+  } catch (error) {
+    throw new Error(`Error deleting tasks: ${error.message}`);
+  }
 };
