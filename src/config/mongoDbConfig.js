f4bacc4b159fa33c16c5370855029adc26c4de8c
Bikal <bikaladhikari5@gmail.com>
2024-04-25 16:40:14 +1000

    timeout configuration in your MongoDB connection using Mongoose

--- src/config/mongoDbConfig.js
+++ src/config/mongoDbConfig.js
@@ -2,8 +2,15 @@ import mongoose from "mongoose";
 
 export const connectMongo = () => {
   const url = "mongodb://localhost:27017/blog";
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 30000, // Adjust the connection timeout (default is 30000 milliseconds)
+    socketTimeoutMS: 45000, // Adjust the socket timeout (default is 45000 milliseconds)
+  };
+
   mongoose
-    .connect(url)
+    .connect(url, options)
     .then(() => console.log("DB connected!"))
     .catch((err) => console.error(err));
 };
