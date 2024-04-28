1c8d0fa50fc4067117a69da45fa1be3338271b53
Bikal <bikaladhikari5@gmail.com>
2024-04-28 17:43:53 +1000

    Ready for deployment purpose

--- src/config/mongoDbConfig.js
+++ src/config/mongoDbConfig.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 export const connectMongo = () => {
-  const url = "mongodb://localhost:27017/blog";
+  // const url = "mongodb://localhost:27017/blog";
   mongoose
-    .connect(url)
+    .connect(process.env.MONGODB_URL)
     .then(() => console.log("DB connected!"))
     .catch((err) => console.error(err));
 };
